23dece5c5b6edde26551da3d4f318b77dc314d1b
pranalipatil209 <pranalipatil209@gmail.com>
2016-06-01 19:19:47 +0530

    ng-enter added

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -1,8 +1,15 @@
 angular.module('attendanceApp').controller('attendanceCtrl',function($scope){
+    //$scope.Datas = [];
     $scope.isActive = true;
     $scope.isDisabled = false;
     $scope.chngClass = function(){
         $scope.isActive = !$scope.isActive;
         $scope.isDisabled = !$scope.isDisabled;
     };
+    //$scope.submitData = function(){
+    //    if(this.myText) {
+    //        $scope.Datas.push($scope.myText);
+    //        $scope.myText = "";
+    //    }
+    //}
 });
